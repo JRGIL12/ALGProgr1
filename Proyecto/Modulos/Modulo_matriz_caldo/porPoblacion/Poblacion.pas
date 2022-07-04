fb17d4d0f4f691360e8cf74f8899f2c1b110d846
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-04 19:05:31 -0400

    Modulo Por poblacion
    El random causaba error al inicializarse en 0

--- Proyecto/Modulos/Modulo_matriz_caldo/porPoblacion/Poblacion.pas
+++ Proyecto/Modulos/Modulo_matriz_caldo/porPoblacion/Poblacion.pas
@@ -13,13 +13,13 @@ program PorPoblacion;
 	while z<> poblacion do
 		Begin
 			Randomize;
-			x:= random(11);
-			Randomize; //Probablemente este demas
-			y:= random(11);
+			x:=1+random(10);
+			y:=1+random(10);
 			If Caldo[x,y]<>1 then
 				begin
 					Caldo[x,y]:=1;
 					z:=z+1;
 				end;
-		End; readln();
+		End;
+   readln();
   End.
