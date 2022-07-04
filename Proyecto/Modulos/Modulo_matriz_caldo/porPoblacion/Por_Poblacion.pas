7ddca486dc05df2c59c9392db5feccb3d12087f1
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-04 18:54:13 -0400

    Porpoblacion
    Areeglado, se le modifico el valor x,y con un +1 de forma que el valor nunca de 0 y genere error

--- Proyecto/Modulos/Modulo_matriz_caldo/porPoblacion/Por_Poblacion.pas
+++ Proyecto/Modulos/Modulo_matriz_caldo/porPoblacion/Por_Poblacion.pas
@@ -3,8 +3,7 @@ program PorPoblacion;
    Matriz= array [1..10,1..10] of integer;  
  var
 	Caldo:matriz;
-	poblacion,x,y,z: integer;
-	
+	poblacion,x,y,z,Salir: integer;
  Begin
     Read(poblacion);
 	z:=0;
@@ -13,12 +12,11 @@ program PorPoblacion;
 		for y := 1 to 10 do 
 			Caldo[x,y]:=0;
 	//Registramos las celulas
+	Randomize;
 	while z<> poblacion do
 		Begin
-			Randomize;
-			x:= random(11);
-			Randomize; //Probablemente este demas
-			y:= random(11);
+			x:=1+random(10);
+			y:=1+random(10);
 			If Caldo[x,y]<>1 then
 				begin
 					Caldo[x,y]:=1;
@@ -26,14 +24,15 @@ program PorPoblacion;
 				end;
 		End;
 	//Imprime el caldo de cultivo
-  for x := 1 to 10 do
-    begin
-      for y := 1 to 10 do 
+    for x := 1 to 10 do
         begin
-          write('|');
-          write(Caldo[x,y]);
+             for y := 1 to 10 do
+                 begin
+                      write('|');
+                      write(Caldo[x,y]);
+
+                 end;
+             writeln('|');
         end;
-      writeln('|');
-    end;
-    readln
+    readln(Salir);
   End.
