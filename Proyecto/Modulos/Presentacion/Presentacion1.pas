0ca752952381e5a25faffe14e2611752af1f6656
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-05 08:47:09 -0400

    Presentacion

---
+++ Proyecto/Modulos/Presentacion/Presentacion1.pas
@@ -0,0 +1,16 @@
+Program Presentacion;
+ {Intento de presentacion 1}
+Uses
+ crt;
+
+
+Begin
+ writeln('');writeln('');writeln('');//Tres lineas
+ write('  ');textbackground(red);write('  ');textbackground(0);writeln(' ');     //
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ write(' ');textbackground(red);write('    ');textbackground(0);writeln(' ');textbackground(red);textbackground(0);
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+
+ Readln;
+End.
