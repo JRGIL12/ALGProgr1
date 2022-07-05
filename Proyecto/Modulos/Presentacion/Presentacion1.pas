6152947a8ab6889ed45aaad44d43fd66a968ef59
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-05 09:01:58 -0400

    Presentacion
    Logo ACA

--- Proyecto/Modulos/Presentacion/Presentacion1.pas
+++ Proyecto/Modulos/Presentacion/Presentacion1.pas
@@ -5,12 +5,28 @@ Uses
 
 
 Begin
- writeln('');writeln('');writeln('');//Tres lineas
- write('  ');textbackground(red);write('  ');textbackground(0);writeln(' ');     //
+ //Linea 0
+ writeln('');
+ //Linea 1
+ write('  ');textbackground(red);write('  ');textbackground(0);write(' ');
+ write('  ');textbackground(red);write('   ');textbackground(0);write(' ');
+ write(' ');textbackground(red);write('  ');textbackground(0);writeln(' ');
+ //Linea 2
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
+ write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
  write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
- write(' ');textbackground(red);write('    ');textbackground(0);writeln(' ');textbackground(red);textbackground(0);
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ //Linea 3
+ write(' ');textbackground(red);write('    ');textbackground(0);
+ write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
+ write(' ');textbackground(red);writeln('    ');textbackground(0);
+ //Linea4
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
+ write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
+  write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ //Linea 5
+ write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
+ write('  ');textbackground(red);write('   ');textbackground(0);write(' ');
+ textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
 
  Readln;
 End.
