aa9b9b28307406fef4994a182d448e349a107213
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-05 09:40:52 -0400

    Presentacion
    se remplaza todos los textbackground(0) por normVideo, a razon de mayor legibilidad

--- Proyecto/Modulos/Presentacion/Presentacion1.pas
+++ Proyecto/Modulos/Presentacion/Presentacion1.pas
@@ -8,25 +8,25 @@ Begin
  //Linea 0
  writeln('');
  //Linea 1
- write('  ');textbackground(red);write('  ');textbackground(0);write(' ');
- write('  ');textbackground(red);write('   ');textbackground(0);write(' ');
- write(' ');textbackground(red);write('  ');textbackground(0);writeln(' ');
+ write('  ');textbackground(red);write('  ');NormVideo;write(' ');
+ write('  ');textbackground(red);write('   ');NormVideo;write(' ');
+ write(' ');textbackground(red);write('  ');NormVideo;writeln(' ');
  //Linea 2
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
- write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ write(' ');textbackground(red);write(' ');NormVideo;;write('  ');textbackground(red);write(' ');NormVideo;
+ write(' ');textbackground(red);write(' ');NormVideo;write('   ');
+ write(' ');textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);writeln(' ');NormVideo;
  //Linea 3
- write(' ');textbackground(red);write('    ');textbackground(0);
- write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
- write(' ');textbackground(red);writeln('    ');textbackground(0);
+ write(' ');textbackground(red);write('    ');NormVideo;
+ write(' ');textbackground(red);write(' ');NormVideo;write('   ');
+ write(' ');textbackground(red);writeln('    ');NormVideo;
  //Linea4
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
- write(' ');textbackground(red);write(' ');textbackground(0);write('   ');
-  write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);writeln(' ');textbackground(0);
+ write(' ');textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);write(' ');NormVideo;
+ write(' ');textbackground(red);write(' ');NormVideo;write('   ');
+  write(' ');textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);writeln(' ');NormVideo;
  //Linea 5
- write(' ');textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);
- write('  ');textbackground(red);write('   ');textbackground(0);write(' ');
- textbackground(red);write(' ');textbackground(0);write('  ');textbackground(red);write(' ');textbackground(0);write(' ');
+ write(' ');textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);write(' ');NormVideo;
+ write('  ');textbackground(red);write('   ');NormVideo;write(' ');
+ textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);write(' ');NormVideo;write(' ');
  //Continuacion de la linea 5
  textbackground(white);textcolor(red);writeln(' Automata Celular alive');
 
