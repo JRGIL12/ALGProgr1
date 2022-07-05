6365ba774b608b24fa361d79f45cfc7aeb8064d8
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-05 10:23:14 -0400

    Presentacion A
    + descipcion

--- Proyecto/Modulos/Presentacion/Presentacion1.pas
+++ Proyecto/Modulos/Presentacion/Presentacion1.pas
@@ -28,9 +28,21 @@ Begin
  write('  ');textbackground(red);write('   ');NormVideo;write(' ');
  textbackground(red);write(' ');NormVideo;write('  ');textbackground(red);write(' ');NormVideo;write(' ');
  //Continuacion de la linea 5
- textbackground(white);textcolor(red);writeln(' Automata Celular alive');
-
- NormVideo;
+ textbackground(white);textcolor(red);writeln(' Automata Celular alive');NormVideo;
+ //linea 6-7
+ writeln();
+ textcolor(red);Writeln('////////////////////////////////////////////////////////////////////////////////');NormVideo;
+ //Linea 8: descripcion
+ write('La evolucion de la vida tiene una variedad de patrones reconocidos que provienen');
+ Write('de determinadas posiciones iniciales. La vida es un ejemplo de emergencia y auto-organizacion.');
+ Write(' Los automatas celulares son automatas simples que producen una   salida a partir de varias ');
+ Write('entradas, modificando en el proceso su estado segun   una funcion de transicin. ');
+ Write('Por lo general, en un automata celular, al igual que  en la evolucion, el estado de ');
+ Write('una celula en una generacion determinada depende  unica y exclusivamente de los estados ');
+ writeln('de las celulas vecinas y de su propio     estado en la generacion anterior.');
+ writeln();
+ // linea
+ textcolor(red);Writeln('////////////////////////////////////////////////////////////////////////////////');NormVideo;
 
  Readln;
 End.
