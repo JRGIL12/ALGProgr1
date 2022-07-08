ad6b2d2d6ff9a27eb8ab3379ea55c239d7365c68
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-08 08:22:44 -0400

    Menu
    Menu bonito

--- Proyecto/Modulos/Menu/Menu.pas
+++ Proyecto/Modulos/Menu/Menu.pas
@@ -5,11 +5,19 @@ uses
 
 
 Begin
- writeln('');writeln('');writeln('');{tres saltos de linea para intentar centrarlo }
+ writeln();writeln();writeln();writeln();writeln();{5 saltos de linea para intentar centrarlo }
  Write('                         ');{ centrar}
  Textbackground(7);textcolor(3+blink);Writeln(' Automata Celular Alive ');normvideo;
  Write('                         ');
  Write('         ');Textbackground(7);textcolor(0);Writeln(' MENU ');normvideo;
- Writeln(' ');
+ Writeln();
+ Writeln();
+ Write('                               ');write(1);
+ Textbackground(7);textcolor(3);Writeln('   Play   ');normvideo;Writeln();
+ Write('                               ');write(2);
+ Textbackground(7);textcolor(3);Writeln(' Sentings ');normvideo;Writeln();
+ Write('                               ');write(3);
+ Textbackground(7);textcolor(3);Writeln('   Exit   ');normvideo;Writeln();
+ writeln();Write('                               ');
  Readln;
 End.
