71a167fb64e858a8a45adcbedee97f3fc9fdbb6b
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-08 08:05:32 -0400

    Menu
    encabezado del menu

---
+++ Proyecto/Modulos/Menu/Menu.pas
@@ -0,0 +1,15 @@
+Program Menu;
+{}
+uses
+ crt;
+
+
+Begin
+ writeln('');writeln('');writeln('');{tres saltos de linea para intentar centrarlo }
+ Write('                         ');{ centrar}
+ Textbackground(7);textcolor(3+blink);Writeln(' Automata Celular Alive ');normvideo;
+ Write('                         ');
+ Write('         ');Textbackground(7);textcolor(0);Writeln(' MENU ');normvideo;
+ Writeln(' ');
+ Readln;
+End.
