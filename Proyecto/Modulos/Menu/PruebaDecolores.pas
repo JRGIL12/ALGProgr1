a6ff4d9b8ce4dcab0c4daec584610ea0d5f1c07e
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2022-07-08 07:44:07 -0400

    Prueba De colores
    probando colores para el menu

---
+++ Proyecto/Modulos/Menu/PruebaDecolores.pas
@@ -0,0 +1,21 @@
+Program Prueba_de_colores;
+{Una prueba de colores para el menu}
+uses
+ crt;
+
+var
+ j,i:integer;
+
+Begin
+ writeln('          ');
+ writeln('          ');
+ writeln('          ');
+ for i:=1 to 9 do
+  for j:=1 to 9 do
+   begin
+    textbackground(i);writeln(i,' fondo      ');
+    textcolor(j);writeln(i,j,' fondo+texto         ');
+    textcolor(j+blink);writeln(i,j,' fondo+texto+blink   ');
+   end;
+ Readln;
+End.
